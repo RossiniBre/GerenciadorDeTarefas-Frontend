8fa86b43ce772ec76701173fcc9cb16d00c995a3
RossiniBre <brenorcarbone@gmail.com>
2026-09-08 17:33:48 -0300

    config api for enviroment variable

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export async function loadTasks(token) {
   const response = await fetch(`${API_URL}/tasks`, {
--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function login(identifier, password) {
   const response = await fetch(`${API_BASE_URL}/users/login`, {
